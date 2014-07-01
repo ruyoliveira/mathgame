3819d4348521875c6d48c71ee8a5500083da0587
Rodrigo de França de Sá Menezes <rodrigo.samenezes@me.com>
2014-07-01 16:34:06 -0300

    cena GameMenu concluida.

---
+++ Assets/ButtonMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonMenu : MonoBehaviour {
+	private TweenRotation tRotation;
+	private TweenScale tScale;
+	// Use this for initialization
+	void Start () 
+	{
+		tRotation = GetComponent<TweenRotation> ();
+		tScale = GetComponent<TweenScale> ();
+	}
+
+	void OnHover(bool over)
+	{
+		if (over)
+		{
+			tRotation.ResetToBeginning();
+			tRotation.PlayForward();
+
+			tScale.ResetToBeginning();
+			tScale.Play();
+		}
+		else
+		{
+			tScale.enabled = false;
+		}
+	}
+
+}
---
+++ Assets/GameMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	public void OnNewGameClick()
+	{
+		Application.LoadLevel ("mosaic");
+	}
+
+	public void OnContinueClick()
+	{
+
+	}
+	public void OnConfigurationClick()
+	{
+
+	}
+	public void OnExitClick()
+	{
+		Application.Quit ();
+	}
+}
