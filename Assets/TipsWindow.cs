cfe2409ab679b7b3200b615d86ff6d7ca87191aa
Rodrigo de França de Sá Menezes <rodrigo.samenezes@me.com>
2014-07-10 14:09:16 -0300

    janela de dicas em desenvolvimento

---
+++ Assets/TipsWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TipsWindow : MonoBehaviour {
+	public UILabel lbText;
+
+	// Use this for initialization
+	public string tipsText
+	{
+		get{
+			return lbText.text;
+		}
+		set{
+			lbText.text = value;
+		}
+	}
+
+	public void ShowWindow()
+	{
+
+	}
+	public void CloseWindow()
+	{
+
+	}
+}
