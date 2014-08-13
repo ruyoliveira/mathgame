bafe8f6264088757bb2645e2c5f2b396772ef679
ruyoliveira <ruygsgoliveira@me.com>
2014-08-13 00:11:44 -0300

    carroussel de selecao de fases implementado

--- Assets/code/scripts/Carroussel.cs
+++ Assets/code/scripts/Carroussel.cs
@@ -4,37 +4,53 @@ using System.Collections;
 public class Carroussel : MonoBehaviour {
 	public Vector2 firstMousePosition;
 	public bool buttonDown;
+	public Transform leftLimit;
+	public Transform rightLimit;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	void Update() {
+		//Verifica se pressionou o botao esquerdo do mouse
 		if(Input.GetMouseButtonDown(0))
 		{
 			buttonDown = true;
 			firstMousePosition = Input.mousePosition;
 		}
+		//Verifica se soltou o botao esquerdo do mouse
 		else if(Input.GetMouseButtonUp(0))
 		{
 			buttonDown = false;
 		}
+		//Move os elementos com relação ao deslocamento do mouse clicado
 		if(buttonDown && (Input.GetMouseButton(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)))
 		{
-			Vector2 actualMousePosition = Input.mousePosition;
-			Vector2 deltaMouse = actualMousePosition - firstMousePosition;
-			deltaMouse.Normalize();
-			transform.Translate(new Vector2(deltaMouse.x/4,0));
-			firstMousePosition = Input.mousePosition;	//modo 1
-			Debug.Log (deltaMouse);
-			
-			//Posicao do mouse enquanto o botao esquero esta clicado
-			//Vector3 touchDeltaPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			
-			//touchDeltaPosition.z = 0;	//zera o eixo z da posicao do mouse
-			//Vector2 deltaMousePosition = touchDeltaPosition;
-			//move o objeto no plano xy
-			//transform.Translate(new Vector2(deltaMousePosition,0));
+			Vector2 actualMousePosition = Input.mousePosition;	//converte posicao atual do mouse em vector2
+			Vector2 deltaMouse = actualMousePosition - firstMousePosition;	//calcula deslocamento do mouse
+			deltaMouse.Normalize();		//normaliza deslocamento
+			//Rotaciona para a esquerda
+			if(deltaMouse.x >0)
+			{
+				//Nao rotaciona se o limite direito ja tiver sido atingido
+				if(transform.position.x < rightLimit.position.x)
+				{
+					transform.Translate(new Vector2(deltaMouse.x/4,0));
+					firstMousePosition = Input.mousePosition;	//modo 1
+				}
+			}
+			//Rotaciona para a direita
+			else
+			{
+				//Nao rotaciona se o limite esquerdo tiver sido atingido
+				if(transform.position.x > leftLimit.position.x)
+				{
+					transform.Translate(new Vector2(deltaMouse.x/4,0));
+					firstMousePosition = Input.mousePosition;	//modo 1
+				}
 
+			}
+
+			//Debug.Log (deltaMouse);
 		}
 	}
 }
---
+++ Assets/code/scripts/VisibilityEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisibilityEvent : MonoBehaviour {
+
+	public EventDelegate becameInvisible;
+	public EventDelegate becameVisible;
+	void OnBecameInvisible()
+	{
+		becameInvisible.Execute();
+	}
+	void OnBecameVisible()
+	{
+		becameVisible.Execute();
+	}
+}
