111b2d987c244d9f4b8e19b335448ffc4699e00a
ruyoliveira <ruygsgoliveira@me.com>
2014-08-26 11:51:43 -0300

    carregando os levels utilizando a nomenclatura w[numeromundo]s[numerodafase]

---
+++ Assets/code/scripts/LevelMosaicManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelMosaicManager : MonoBehaviour {
+	public Button[] levelButtons;
+	public int worldNumber;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void LoadLevel(int levelNumber)
+	{
+		Application.LoadLevelAsync("w"+worldNumber+"s"+levelNumber);
+	}
+}
